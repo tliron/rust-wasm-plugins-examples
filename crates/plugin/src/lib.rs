167319e33a9d07047a372b485aa714c8c0208d2a
Tal Liron <tal.liron@gmail.com>
2025-01-07 03:25:57 -0600

    Small improvements

--- crates/plugin/src/lib.rs
+++ crates/plugin/src/lib.rs
@@ -2,13 +2,18 @@ wit_bindgen::generate!(in "../../assets/wit/acme-plugins.wit");
 
 use {acme::plugins::host, exports::acme::plugins::prettify_plugin};
 
+// This is our implementation of the "prettify" plugin type (=WIT world)
+
+// We shall make the content pretty by quoting all words
+// Gorgeous!
+
 pub struct QuotePrettifyPlugin;
 
 impl prettify_plugin::Guest for QuotePrettifyPlugin {
     // Note: these are not "std::string:String", but "alloc::String"!
-    fn prettify(name: String) -> String {
+    fn prettify(content: String) -> String {
         host::log("thank you for using the quote prettify plugin!");
-        let words = name.split(" ");
+        let words = content.split(" ");
         let words: Vec<String> = words.map(|word| format!("{:?}", word)).collect();
         words.join(" ")
     }
