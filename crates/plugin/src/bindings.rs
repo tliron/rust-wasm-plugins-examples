a8bbb5cef7bc35af33a6eb8655b9aa3f84ba7062
felix <florianfelixmeyer@gmail.com>
2025-04-07 17:38:27 +0200

    update dependencies and add IoView trait

--- crates/plugin/src/bindings.rs
+++ crates/plugin/src/bindings.rs
@@ -1,6 +1,7 @@
 // Note that we are using the full notation of the macro in order
 // to add some options that allow the bindings to live in this independent file
 
+#[allow(clippy::crate_in_macro_def)]
 wit_bindgen::generate!({
     path: "../../assets/wit/acme-plugins.wit",
     default_bindings_module: "crate::bindings",
