677bd0b9ae91ac56f41d480a11bb03bf24afa42e
Tal Liron <tal.liron@gmail.com>
2025-11-23 04:05:27 -0600

    New bindings format

--- host/src/bindings.rs
+++ host/src/bindings.rs
@@ -3,7 +3,7 @@
 wasmtime::component::bindgen!({
     path: "../assets/wit/acme-plugins.wit",
     with: {
-        "acme:plugins/host/map-resource": super::host::Map,
+        "acme:plugins/host.map-resource": super::host::Map,
     },
     imports: { default: trappable },
 });
